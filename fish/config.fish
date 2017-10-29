cac6511f5e9616bc896dd6dbf845e49f2e515cf7
Joni Nevalainen <joninevalainen@gmail.com>
2017-10-29 18:21:03 +0200

    Add fish config

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+set -g fish_user_paths "/usr/local/opt/sbt@0.13/bin" $fish_user_paths
+set -g fish_user_paths "/Users/joni.nevalainen/Library/Python/3.6/bin" $fish_user_paths
+set -x NVM_DIR $HOME/.nvm
+set -x EDITOR vi
+nvm use node
