5c4a8d281fae3f30c8e9d51a9fa1036695a216da
Joni Nevalainen <joninevalainen@gmail.com>
2020-01-09 13:35:17 +0200

    Add fish aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,1 +1,12 @@
+alias cat=bat
+alias preview="fd --type f | fzf --preview 'bat --color \"always\" {}'"
+alias help=tldr
+
+alias g="git"
+alias gc="git commit"
+alias gcan="git commit --amend --no-edit"
+alias gs="git status"
+alias gl="git log"
+alias gd="git diff"
+alias gds="git diff --staged"
 
