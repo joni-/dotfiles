c016c86289542e9bcf50fd5fb7bd00ba59787908
Joni Nevalainen <joninevalainen@gmail.com>
2020-01-25 11:41:43 +0200

    Tweak aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -3,10 +3,12 @@ alias preview="fd --type f | fzf --preview 'bat --color \"always\" {}'"
 alias help=tldr
 
 alias g="git"
-alias gc="git commit"
-alias gcan="git commit --amend --no-edit"
+alias gco="git commit"
+alias gcoan="git commit --amend --no-edit"
 alias gs="git status"
 alias gl="git log"
 alias gd="git diff"
 alias gds="git diff --staged"
+
+alias c="clear"
 
