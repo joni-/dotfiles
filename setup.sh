96ab33d3552980002c7c4203d37d210ff1a36a39
Joni Nevalainen <joninevalainen@gmail.com>
2023-04-27 20:50:32 +0300

    git email and username

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ ln -sf $WORKING_DIR/fish/aliases.fish  ~/.config/fish/aliases.fish
 ln -sf $WORKING_DIR/fish/config.fish  ~/.config/fish/config.fish
 ln -sf $WORKING_DIR/tmux/.tmux.conf  ~/.tmux.conf
 ln -sf $WORKING_DIR/.psqlrc  ~/.psqlrc
+
+git config --global user.name "Joni Nevalainen"
+git config --global user.email "joninevalainen@gmail.com"
