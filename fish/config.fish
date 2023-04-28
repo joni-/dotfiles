14ba504001bc54b3b8232994b3a779425f52b2dc
Joni Nevalainen <joninevalainen@gmail.com>
2023-04-28 19:30:06 +0300

    Add cargo binaries to path

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@
 
 fish_add_path /Users/joni/.local/bin/
 fish_add_path /usr/local/opt/libpq/bin/
+fish_add_path $HOME/.cargo/bin
 set -x NVM_DIR $HOME/.nvm
 set -x EDITOR vi
 set -x SAM_CLI_TELEMETRY 0
