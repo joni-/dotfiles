c43139a885829b10dc9031f8736d27f70d454c3d
Joni Nevalainen <joninevalainen@gmail.com>
2023-05-04 14:56:34 +0300

    Update fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,13 @@
 fish_add_path /Users/joni/.local/bin/
 fish_add_path /usr/local/opt/libpq/bin/
 fish_add_path $HOME/.cargo/bin
+fish_add_path /opt/homebrew/bin
+fish_add_path $HOME/Library/Python/3.9/bin/
 set -x NVM_DIR $HOME/.nvm
 set -x EDITOR vi
 set -x SAM_CLI_TELEMETRY 0
+set -x PYENV_ROOT $HOME/.pyenv
 
+if test -e ~/.config/fish/config.local.fish
+    . ~/.config/fish/config.local.fish
+end
