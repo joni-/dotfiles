e7390f85fd1bd36d0a4ad81d740a146dbefaf18c
Joni Nevalainen <joninevalainen@gmail.com>
2024-11-06 20:31:52 +0200

    Drop obsolete functions

--- fish/functions/node.fish
+++
@@ -1,9 +0,0 @@
-function node
-    if which node
-      eval (which node) $argv
-    else
-      nvm use node
-      eval (which node) $argv
-    end
-end
-
--- fish/functions/npm.fish
+++
@@ -1,9 +0,0 @@
-function npm
-    if which npm
-      eval (which npm) $argv
-    else
-      nvm use node
-      eval (which npm) $argv
-    end
-end
-
