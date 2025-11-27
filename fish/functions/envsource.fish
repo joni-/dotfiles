f3401391ef873bb9ba796980448d580663280745
Joni Nevalainen <joninevalainen@gmail.com>
2025-11-27 14:20:09 +0200

    envsource: ignore blank lines

--- fish/functions/envsource.fish
+++ fish/functions/envsource.fish
@@ -1,6 +1,8 @@
 function envsource
   for line in (cat $argv | grep -v '^#')
-    set item (string split -m 1 '=' $line)
-    set -gx $item[1] $item[2]
+    if test -n "$line"
+      set item (string split -m 1 '=' $line)
+      set -gx $item[1] $item[2]
+    end
   end
 end
